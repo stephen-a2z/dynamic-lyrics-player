fa83e30451455c7a49c566746a76b3992a7f7b02
stephen-a2z <stephen.liao@borderxlab.com>
2025-05-27 21:20:25 +0800

    add github workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DynamicLyricsPlayer/', // 修改这里
+  base: '/dynamic-lyrics-player/',
 });
